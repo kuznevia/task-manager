ec492bccc0cdc4facfa99138b8235b68bdd8a925
Viacheslav <vyac.v.kuznetsov@rt.ru>
2023-03-22 18:48:40 +0300

    some other preparations
    1. adding chakra
    2. adding entrances
    3. adding base styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import './index.css';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import { Login } from 'components/Login/Login';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ChakraProvider>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
